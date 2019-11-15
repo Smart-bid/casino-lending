c946160e25dfaa71134501471ae7ebe678eb1faa
youvovk <yuriy3vovk@gmail.com>
2019-11-15 13:25:29 +0200

    some problem

--- Casino_landing/src/App.js
+++ Casino_landing/src/App.js
@@ -19,11 +19,11 @@ function App() {
 
           <div className="main-wrapper">
             <main className="main">
-              <p className="text text_30px">Discover a new world of casino</p>
-                <p className="text text__extra-light text_60px wrapper">
-                  <span className="text-orange">FIRST DEPOSIT BONUS: 200% UP TO £50 +  200 BONUS</span>
+              <p className="text text_24px">Discover a new world of casino</p>
+                <p className="text text__extra-light text_36px wrapper">
+                  FIRST DEPOSIT BONUS: 200% UP TO £50 +  200 BONUS
                 </p>
-              <p className="text text__extra-bold text_30px p_indent"><span className="text-white neon-green">+40 free SPINS</span></p>
+              <p className="text text__extra-bold text_24px p_indent"><span className="text-violet">+40 free SPINS</span></p>
 
               <a href="#" className="register register_big">Register Now</a>
             </main>
