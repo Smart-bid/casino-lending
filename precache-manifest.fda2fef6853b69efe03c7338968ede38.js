4fd91ed71af674bdbc4a607e7eda050b533de0f0
youvovk <yuriy3vovk@gmail.com>
2019-11-19 14:51:34 +0200

    Updates

---
+++ precache-manifest.fda2fef6853b69efe03c7338968ede38.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98020e8c1ec8d0f753a84347f94fdd0f",
+    "url": "/Smart-bid.github.io/casino-lending/index.html"
+  },
+  {
+    "revision": "75f5079d68e70e101260",
+    "url": "/Smart-bid.github.io/casino-lending/static/css/main.7463b9a4.chunk.css"
+  },
+  {
+    "revision": "d462d82071485b9ac599",
+    "url": "/Smart-bid.github.io/casino-lending/static/js/2.080dfb86.chunk.js"
+  },
+  {
+    "revision": "75f5079d68e70e101260",
+    "url": "/Smart-bid.github.io/casino-lending/static/js/main.71132c91.chunk.js"
+  },
+  {
+    "revision": "c1cecb3874c4ea52df28",
+    "url": "/Smart-bid.github.io/casino-lending/static/js/runtime-main.79d2fa5b.js"
+  },
+  {
+    "revision": "4e63df33be450599a80a6fb431e7b1e0",
+    "url": "/Smart-bid.github.io/casino-lending/static/media/back-button-header.4e63df33.png"
+  },
+  {
+    "revision": "85b1412c3013ee85552ec8f19ac3f694",
+    "url": "/Smart-bid.github.io/casino-lending/static/media/back-button-main.85b1412c.png"
+  },
+  {
+    "revision": "1db7c7927bfc2d90b5d465246c35fb1f",
+    "url": "/Smart-bid.github.io/casino-lending/static/media/back-main.1db7c792.jpg"
+  },
+  {
+    "revision": "0fa3ecd81032d1eb144d74a497c43637",
+    "url": "/Smart-bid.github.io/casino-lending/static/media/back-menu.0fa3ecd8.png"
+  },
+  {
+    "revision": "3eb2470d29b8f29790c9e1b2c6c2911d",
+    "url": "/Smart-bid.github.io/casino-lending/static/media/big-cash.3eb2470d.png"
+  },
+  {
+    "revision": "c4ae5727c8d33530cc2857498538ea0e",
+    "url": "/Smart-bid.github.io/casino-lending/static/media/first-cash.c4ae5727.png"
+  },
+  {
+    "revision": "883bc05059da09c2c374ca2b0ac5a858",
+    "url": "/Smart-bid.github.io/casino-lending/static/media/footer-back.883bc050.png"
+  }
+]);
