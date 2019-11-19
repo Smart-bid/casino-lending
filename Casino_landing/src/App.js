58aa816e3fbe8054f45e4912f41c8b78fb0d5ed0
youvovk <yuriy3vovk@gmail.com>
2019-11-19 14:30:39 +0200

    ready all

--- Casino_landing/src/App.js
+++ Casino_landing/src/App.js
@@ -1,69 +1,93 @@
 import React from 'react';
 import './main.scss';
 
-function App() {
-  return (
-    <>
-      <div className="background">
+export class App extends React.Component {
+  state = {
+    headerBack: false,
+  }
 
-        <div className="container">
+  listenScrollEvent = e => {
+    if (window.scrollY > 50) {
+      this.setState({
+        headerBack: true,
+      })
+    } else {
+      this.setState({
+        headerBack: false,
+      })
+    }
+  }
 
-          <header className="header">
-            <div className="header__logo-wrapper">
-              <p className="header__logo">logo</p>
-              <p className="header__logo_bottom">lorem ipsum</p>
+  componentDidMount() {
+    window.addEventListener('scroll', this.listenScrollEvent);
+  }
+
+  render () {
+    const { headerBack } = this.state;
+
+    return (
+      <>
+        <div className="background">
+
+          <div className="container">
+
+            <header className={`header ${headerBack && 'header__background'}`}>
+              <div className="header__logo-wrapper">
+                <p className="header__logo">logo</p>
+                <p className="header__logo_bottom">lorem ipsum</p>
+              </div>
+
+              <input type="button" value="Register Now" className="register" />
+            </header>
+
+            <div className="main-wrapper">
+              <main className="main">
+                <p className="text text_30px">Discover a new world of casino</p>
+                  <p className="text text__extra-light text_60px wrapper">
+                    <span className="text-orange">FIRST DEPOSIT BONUS: 200% UP TO £50 +  200 BONUS</span>
+                  </p>
+                <p className="text text__extra-bold text_30px p_indent"><span className="text-red">+40 free SPINS</span></p>
+
+                <a href="#" className="register register_big">Register Now</a>
+              </main>
             </div>
 
-            <input type="button" value="Register Now" className="register" />
-          </header>
-
-          <div className="main-wrapper">
-            <main className="main">
-              <p className="text text_30px">Discover a new world of casino</p>
-                <p className="text text__extra-light text_60px wrapper">
-                  <span className="text-orange">FIRST DEPOSIT BONUS: 200% UP TO £50 +  200 BONUS</span>
-                </p>
-              <p className="text text__extra-bold text_30px p_indent"><span className="text-red">+40 free SPINS</span></p>
-
-              <a href="#" className="register register_big">Register Now</a>
-            </main>
+          
           </div>
-
-         
         </div>
-      </div>
 
-      <div className="footer-background">
-        <div className="footer-wrapper">
-          <footer className="footer">
-            <div className="footer__top">
-                <img src="./images/visa.png" alt="" />
-                <img src="./images/master-card.png" alt="" />
-                <img src="./images/skrill.png" alt="" />
-                <img src="./images/pay.png" alt="" />
-                <img src="./images/visa.png" alt="" />
-                <img src="./images/master-card.png" alt="" />
-                <img src="./images/skrill.png" alt="" />
-                <img src="./images/pay.png" alt="" />
-                <img src="./images/visa.png" alt="" />
-                <img src="./images/master-card.png" alt="" />
-                <img src="./images/skrill.png" alt="" />
-            </div>
+        <div className="footer-background">
+          <div className="footer-wrapper">
+            <footer className="footer">
+              <div className="footer__top">
+                  <img src="./images/visa.png" alt="" />
+                  <img src="./images/master-card.png" alt="" />
+                  <img src="./images/skrill.png" alt="" />
+                  <img src="./images/pay.png" alt="" />
+                  <img src="./images/visa.png" alt="" />
+                  <img src="./images/master-card.png" alt="" />
+                  <img src="./images/skrill.png" alt="" />
+                  <img src="./images/pay.png" alt="" />
+                  <img src="./images/visa.png" alt="" />
+                  <img src="./images/master-card.png" alt="" />
+                  <img src="./images/skrill.png" alt="" />
+              </div>
 
-            <div className="footer__bottom">
-                <img src="./images/skrill.png" alt="" />
-                <img src="./images/pay.png" alt="" />
-                <img src="./images/visa.png" alt="" />
-                <img src="./images/master-card.png" alt="" />
-                <img src="./images/skrill.png" alt="" />
-                <img src="./images/pay.png" alt="" />
-                <img src="./images/visa.png" alt="" />
-            </div>
-          </footer>
+              <div className="footer__bottom">
+                  <img src="./images/skrill.png" alt="" />
+                  <img src="./images/pay.png" alt="" />
+                  <img src="./images/visa.png" alt="" />
+                  <img src="./images/master-card.png" alt="" />
+                  <img src="./images/skrill.png" alt="" />
+                  <img src="./images/pay.png" alt="" />
+                  <img src="./images/visa.png" alt="" />
+              </div>
+            </footer>
+          </div>
         </div>
-      </div>
-    </>
-  );
+      </>
+   );
+  }
 }
 
 export default App;
