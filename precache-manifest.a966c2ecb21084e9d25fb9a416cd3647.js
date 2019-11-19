c17445c3690866d15ec262b7eefe1fe4c30c9dbc
youvovk <yuriy3vovk@gmail.com>
2019-11-19 15:58:10 +0200

    Updates

---
+++ precache-manifest.a966c2ecb21084e9d25fb9a416cd3647.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7d25b6c5bf459c0ac47ee17ea4f1534",
+    "url": "/Smart-bid.io/casino-lending/index.html"
+  },
+  {
+    "revision": "8fc397ae05445bbbfc7b",
+    "url": "/Smart-bid.io/casino-lending/static/css/main.28093aa1.chunk.css"
+  },
+  {
+    "revision": "2a9ec1f7c21812ac017b",
+    "url": "/Smart-bid.io/casino-lending/static/js/2.a67f9ffc.chunk.js"
+  },
+  {
+    "revision": "8fc397ae05445bbbfc7b",
+    "url": "/Smart-bid.io/casino-lending/static/js/main.8807340f.chunk.js"
+  },
+  {
+    "revision": "a4d07503dc7992c75208",
+    "url": "/Smart-bid.io/casino-lending/static/js/runtime-main.b03852d6.js"
+  },
+  {
+    "revision": "4e63df33be450599a80a6fb431e7b1e0",
+    "url": "/Smart-bid.io/casino-lending/static/media/back-button-header.4e63df33.png"
+  },
+  {
+    "revision": "85b1412c3013ee85552ec8f19ac3f694",
+    "url": "/Smart-bid.io/casino-lending/static/media/back-button-main.85b1412c.png"
+  },
+  {
+    "revision": "1db7c7927bfc2d90b5d465246c35fb1f",
+    "url": "/Smart-bid.io/casino-lending/static/media/back-main.1db7c792.jpg"
+  },
+  {
+    "revision": "0fa3ecd81032d1eb144d74a497c43637",
+    "url": "/Smart-bid.io/casino-lending/static/media/back-menu.0fa3ecd8.png"
+  },
+  {
+    "revision": "3eb2470d29b8f29790c9e1b2c6c2911d",
+    "url": "/Smart-bid.io/casino-lending/static/media/big-cash.3eb2470d.png"
+  },
+  {
+    "revision": "c4ae5727c8d33530cc2857498538ea0e",
+    "url": "/Smart-bid.io/casino-lending/static/media/first-cash.c4ae5727.png"
+  },
+  {
+    "revision": "883bc05059da09c2c374ca2b0ac5a858",
+    "url": "/Smart-bid.io/casino-lending/static/media/footer-back.883bc050.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Smart-bid.github.io/casino-lending/precache-manifest.b29643e21a7b23be98226aec4007e603.js"
+  "/Smart-bid.io/casino-lending/precache-manifest.a966c2ecb21084e9d25fb9a416cd3647.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Smart-bid.github.io/casino-lending/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Smart-bid.io/casino-lending/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
